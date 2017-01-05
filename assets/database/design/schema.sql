40b4a10b9103a04cd02a2cd0f49f4c56b3e1d580
Shrmila <shrmila1811@gmail.com>
2017-01-05 16:44:28 +0800

    Hi i have made some changes.

--- assets/database/design/schema.sql
+++ assets/database/design/schema.sql
@@ -11,6 +11,11 @@ CREATE TABLE `user` (
 CREATE TABLE `activity` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `description` varchar(255) DEFAULT NULL,
-  `create_at`
+  `create_at` varchar(255) DEFAULT NULL,
+  `create_by` varchar(255) DEFAULT NULL,
+  `update_at` varchar(255) DEFAULT NULL,
+  `update_by` varchar(255) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  
   PRIMARY KEY (`id`),
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
