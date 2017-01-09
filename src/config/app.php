a6c38f234c8554a562b3672c335c7c21ab4aef0d
Phartheben <Pharz@Pharthebens-MacBook-Pro.local>
2017-01-09 12:24:52 +0800

    added codes in schema

--- src/config/app.php
+++ src/config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // vendors
+        Laravel\Passport\PassportServiceProvider::class,
     ],
 
     /*
