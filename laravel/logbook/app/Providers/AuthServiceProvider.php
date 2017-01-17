24a4cc7561b7308d6707853bca3b4a438fc1b862
Phartheben <Pharz@Pharthebens-MacBook-Pro.local>
2017-01-17 09:51:24 +0800

    deleted some files and updated

--- laravel/logbook/app/Providers/AuthServiceProvider.php
+++ laravel/logbook/app/Providers/AuthServiceProvider.php
@@ -7,6 +7,7 @@ use Laravel\Passport\Passport;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Carbon\Carbon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -29,7 +30,9 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
-
-        //
+        Passport::tokensExpireIn(Carbon::now()->addDays(15));
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
+        Passport::enableImplicitGrant();
     }
 }
+ 
